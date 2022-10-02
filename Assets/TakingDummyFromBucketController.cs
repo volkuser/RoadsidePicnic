20411a6a742faf8103e9da02be8ac77abe48d73e
daniel demidov <demidov008h@gmail.com>
2022-10-02 09:15:17 +0300

    storages

---
+++ Assets/TakingDummyFromBucketController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class TakingDummyFromBucketController : MonoBehaviour
+{
+    private bool _playerDetect;
+    [SerializeField] private float width;
+    [SerializeField] private float height;
+    [SerializeField] private Transform groundPosition;
+    [SerializeField] private LayerMask whatIsPlayer;
+
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Sprite withoutDummy;
+
+    private void Awake() => _spriteRenderer = GetComponent<SpriteRenderer>();    
+    
+    private void Update()
+    {
+        _playerDetect = Physics2D.OverlapBox(groundPosition.position, 
+            new Vector2(width, height), 0, whatIsPlayer);
+
+        if (!_playerDetect) return;
+        if (!Input.GetKeyDown(KeyCode.E)) return;
+        if (PlayerInventory.hasSecondDummy) return;
+        PlayerInventory.hasSecondDummy = true;
+        _spriteRenderer.sprite = withoutDummy;
+    }
+    
+    private void OnDrawGizmosSelected() 
+    {
+        Gizmos.color = Color.magenta;
+        var position = groundPosition.position;
+        Gizmos.DrawWireCube(position, new Vector2(width, height));  
+    }
+}
